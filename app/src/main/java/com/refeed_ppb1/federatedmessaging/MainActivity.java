09b16ca92793d2b64238e82cfb5b08136f7e26ef
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-05-24 11:52:53 +0700

    Initial commit

---
+++ app/src/main/java/com/refeed_ppb1/federatedmessaging/MainActivity.java
@@ -0,0 +1,14 @@
+package com.refeed_ppb1.federatedmessaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
