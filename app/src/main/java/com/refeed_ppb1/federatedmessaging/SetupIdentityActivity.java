e0ca4dfe5cb78a8d2cb69e1404df94738d08f59e
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-06-19 12:05:28 +0700

    SetupIdentity: Save the password in SharedPreferences
    Closes https://github.com/refeed/FederatedMessagingAndroid/issues/4

--- app/src/main/java/com/refeed_ppb1/federatedmessaging/SetupIdentityActivity.java
+++ app/src/main/java/com/refeed_ppb1/federatedmessaging/SetupIdentityActivity.java
@@ -3,10 +3,19 @@ package com.refeed_ppb1.federatedmessaging;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SetupIdentityActivity extends AppCompatActivity {
+
+    public static final String PASSWORD_PREF_KEY = "password";
+
+    SharedPreferences sharedPreferences;
+    private TextInputEditText passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +23,31 @@ public class SetupIdentityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setup_identity);
         getSupportActionBar().setTitle("Setup identity");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        passwordET = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences(
+                BuildConfig.APPLICATION_ID + R.string.IDENTITY_PREF,
+                MODE_PRIVATE);
+
+        final String savedPassword = sharedPreferences.getString(PASSWORD_PREF_KEY, null);
+        if (savedPassword == null)
+            return;
+        passwordET.setText(savedPassword);
     }
 
     public void setIdentity(View view) {
-        Intent setIdentity = new Intent(SetupIdentityActivity.this,MainActivity.class);
-        startActivity(setIdentity);
+        final String password = passwordET.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(PASSWORD_PREF_KEY, password);
+        editor.commit();
+
+        Toast
+                .makeText(
+                        SetupIdentityActivity.this,
+                        "Successfully set the identity", Toast.LENGTH_SHORT)
+                .show();
+        super.onBackPressed();
     }
 }
